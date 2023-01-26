d62259c8a5fdbcfbd13204b200ae2921703bb7dc
Lucca Araujo <lucca.ara7@gmail.com>
2023-01-25 18:20:12 -0800

    added logic to the account page

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -20,16 +20,39 @@ class Account extends Component{
             token: this.props.auth.token,
         }
         const resp = await Axios.post(accountUrl, data);
+        console.log(resp.data);
+        let pastBookings = [];
+        let upcomingBookings = [];
+        resp.data.forEach(booking => {
+            const today = moment();
+            const checkOutDate = checkOutDate.diff(today, "days");
+            if(diffDays < 0){
+                pastBookings.push(booking);
+            } else{
+                upcomingBookings.push(booking);
+            }
+        });
+        this.setState({
+            pastBookings,
+            upcomingBookings,
+        })
     }
     render(){
+        const { pastBookings, upcomingBookings } = this.state;
         return(
             <div className="account cointainer-fluid">
                 <AccountSideBar />
                 <div className="row">
                     <div className="col s8 offset-s3">
                         <Route exact path="/account" render={()=> <h1> Choose an option on the left</h1>} />
-                        <Route exact path="/account/reservations/confirmed" component={Bookings} />
-                        <Route exact path="/account/reservations/past" component={Bookings} />
+
+
+                        <Route exact path="/account/reservations/confirmed" render={()=>
+                        <Bookings type="upcoming" bookings={upcomingBookings} />} />
+
+                        <Route exact path="/account/reservations/past">
+                            <Bookings type="past" bookings={pastBookings} />
+                        </Route>
                         <Route exact path="/account/reservations/change-pass" component={ChangePassword} />
                     </div>
                 </div>
